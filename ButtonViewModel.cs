53772bf759add278a6cf2c5219cd8bb274c86893
CassioJhones <cassio.bjhones@gmail.com>
2024-03-22 23:18:50 -0300

    Verificacao da API ao iniciar

---
+++ ButtonViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TimeVersion
+{
+    public class ButtonViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private SolidColorBrush _buttonBackground = Brushes.Wheat;
+        public SolidColorBrush ButtonBackground
+        {
+            get { return _buttonBackground; }
+            set
+            {
+                _buttonBackground = value;
+                OnPropertyChanged(nameof(ButtonBackground));
+            }
+        }
+
+        private SolidColorBrush _buttonForeground = Brushes.Black;
+        public SolidColorBrush ButtonForeground
+        {
+            get { return _buttonForeground; }
+            set
+            {
+                _buttonForeground = value;
+                OnPropertyChanged(nameof(ButtonForeground));
+            }
+        }
+
+        private string _buttonContent="Buscar Cidade";
+        public string ButtonContent
+        {
+            get { return _buttonContent; }
+            set
+            {
+                _buttonContent = value;
+                OnPropertyChanged(nameof(ButtonContent));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,8 +13,8 @@ namespace TimeVersion
     public partial class MainWindow : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private const string ApiKey = "f24e3b3b88dc280e84e540c4500113d5";
         private readonly HttpClient _httpClient;
-
         private string _apiStatus = "Online";
         public string ApiStatus
         {
@@ -36,14 +36,19 @@ namespace TimeVersion
 
             if (ApiKey.Length < 32 || string.IsNullOrEmpty(ApiKey))
                 ApiStatus = "Offline";
+            Loaded += MainWindow_Loaded;
         }
-
-        protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
+        
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await CheckApiStatus();
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
         private async void BuscarPrevisaoClick(object sender, RoutedEventArgs evento)
         {
             string cidade = txtCidade.Text.Trim();
-
             try
             {
                 string previsao = await ObterPrevisaoDoTempo(cidade);
@@ -58,7 +63,6 @@ namespace TimeVersion
             {
                 string mensagemErro = ex.Message.Contains("Unauthorized") ? "Falha na API" : ex.Message.Contains("Not Found") ? "Cidade Inexistente" : "Campo Vazio";
                 AtualizarBotaoConcluido((Button)sender, false, mensagemErro);
-
             }
         }
 
@@ -77,7 +81,6 @@ namespace TimeVersion
 
             return $"{cidadeEscolhida.Nome} - {cidadeEscolhida.Sys.Pais}\nTemperatura: {cidadeEscolhida.Main.Temperatura}°C\nSensação: {cidadeEscolhida.Main.SensacaoTermica}°C\nVento: {velocidadeVento} Km/h\nUmidade: {umidade}%";
         }
-
         private async Task<string> ObterDescricaoDoCeu(string cidade)
         {
             string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={ApiKey}&units=metric&lang=pt";
@@ -95,7 +98,6 @@ namespace TimeVersion
 
             return titulo;
         }
-
         private void AtualizarBotaoConcluido(Button botao, bool sucesso, string mensagemErro = "")
         {
             Brush corFundo = sucesso ? new SolidColorBrush(Color.FromRgb(22, 120, 48)) : new SolidColorBrush(Color.FromRgb(145, 10, 10));
@@ -103,12 +105,34 @@ namespace TimeVersion
             botao.Foreground = Brushes.Wheat;
             botao.Content = sucesso ? "Concluído" : mensagemErro;
         }
-
         private void Texto_SairComMouse(object sender, MouseEventArgs e)
         {
             ((Button)sender).Background = Brushes.WhiteSmoke;
             ((Button)sender).Foreground = Brushes.Black;
             ((Button)sender).Content = "Buscar Local";
+        }
+        private async Task CheckApiStatus()
+        {
+            HttpResponseMessage response = await GetApi();
+            if (!response.IsSuccessStatusCode)
+                ApiStatus = "Offline";
+        }
+        private async Task<HttpResponseMessage> GetApi()
+        {
+            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q=brasil&appid={ApiKey}&units=metric&lang=pt";
+            HttpResponseMessage response = null;
+
+            try
+            {
+                response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException)
+            {
+                
+            }
+
+            return response;
         }
     }
 }
